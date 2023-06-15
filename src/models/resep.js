32f1b56b9ee5911af178b4cae7ac7109a8451e11
IqbalAlFakhri <iqbalalfakhri3@gmail.com>
2023-06-15 08:35:44 +0700

    updating model resep

--- src/models/resep.js
+++ src/models/resep.js
@@ -1,13 +1,13 @@
 const dbPool = require('../config/database-bahan');
 
 const getResepByCategori = (body) => {
-    const SQLquery = `SELECT Title, Ingredients, Steps, Loves FROM dataset_resep WHERE category = "${body.category}";`;
+    const SQLquery = `SELECT * FROM dataset_resep WHERE category = "${body.category}";`;
 
     return dbPool.execute(SQLquery);
 };
 
 const getResepByLove = (body) => {
-    const SQLquery = `SELECT Title, Ingredients, Steps, Loves FROM dataset_resep WHERE Loves >= ${body.Loves}`;
+    const SQLquery = `SELECT * FROM dataset_resep WHERE Loves >= ${body.Loves}`;
 
     return dbPool.execute(SQLquery);
 };
